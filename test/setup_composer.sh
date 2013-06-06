8eae9b45ef00d86a60c6c4938e2bc17e5c2c2168
Lars Vierbergen <vierbergenlars@gmail.com>
2013-06-06 08:54:50 +0200

    [test] Fix travis & composer setup

--- test/setup_composer.sh
+++ test/setup_composer.sh
@@ -1,5 +1,6 @@
 if [ "$TRAVIS_SECURE_ENV_VARS" = "true" ];
 then
+	mkdir ~/.composer/
 	echo '{ "config": {"github-oauth":{"github.com": ' > ~/.composer/config.json
 	echo "\"$GH_OAUTH\"" >> ~/.composer/config.json
 	echo '}}}' >> ~/.composer/config.json
